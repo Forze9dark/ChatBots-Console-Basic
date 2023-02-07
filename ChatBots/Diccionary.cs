3296cd4527b9ad320c32015341f4a9a7653edc8f
Carlos Sanchez <carlos.sanchez.despero@gmail.com>
2023-02-07 13:15:02 -0400

    Add project files.

---
+++ ChatBots/Diccionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBots
+{
+    internal class Diccionary
+    {
+
+        // Palabras a aprender.
+        public static Dictionary<string, Func<string>> conocimientos = new Dictionary<string, Func<string>>()
+        {
+            { "hora", () => "La hora actual es " + DateTime.Now.ToString("h:mm tt") },
+            { "dia", () => "Hoy es " + DateTime.Now.ToString("dddd, dd MMMM yyyy") },
+            { "cual es tu nombre", () => "Soy un chatbot creado por OpenAI." },
+            { "defecto", () => "Lo siento, no entiendo la pregunta." },
+            { "abre el navegador", () => {
+                string chromePath = Path.Combine(Environment.GetEnvironmentVariable("ProgramFiles"), "Google\\Chrome\\Application\\chrome.exe");
+                Process.Start(chromePath, "https://www.google.com");
+                return "Abriendo Google Chrome";
+            }}
+
+        };
+
+    }
+}
---
+++ ChatBots/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using ChatBots;
+
+namespace ChatBots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Bienvenido a la IA, ¿en qué puedo ayudarte?");
+            while (true)
+            {
+                Console.Write("Pregunta: ");
+                string pregunta = Console.ReadLine();
+                if (Diccionary.conocimientos.ContainsKey(pregunta))
+                {
+                    Console.WriteLine("Respuesta: " + Diccionary.conocimientos[pregunta]());
+                }
+                else
+                {
+                    Console.WriteLine("Lo siento, no tengo la respuesta a esa pregunta.");
+                    Console.Write("¿Quieres agregar una respuesta a esta pregunta? (s/n) ");
+                    string agregar = Console.ReadLine();
+                    if (agregar == "s")
+                    {
+                        Console.Write("Respuesta: ");
+                        string respuesta = Console.ReadLine();
+                        Diccionary.conocimientos.Add(pregunta, () => respuesta);
+                        Console.WriteLine("La pregunta y respuesta se han agregado a mi base de conocimiento.");
+                    }
+                }
+            }
+
+
+
+        }
+    }
+}
